c6a05cfcec4a0bed39b94621993de7ebcc113ca3
Aron <aronesadegh@gmail.com>
2023-07-31 21:15:43 +0330

    add update question graphQL

--- quiz_app/schema.py
+++ quiz_app/schema.py
@@ -70,5 +70,34 @@ class CreateQuestion(graphene.Mutation):
         return CreateQuestion(is_created=is_created)
 
 
+class UpdateQuestion(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID(required=True)
+        inputs = QuestionInputs()
+
+    is_updated = graphene.Boolean(default_value=False)
+
+    @classmethod
+    def mutate(cls, parent, info, id, inputs=None):
+        if not info.context.user.is_authenticated:
+            raise GraphQLError('you are not authenticated!')
+        elif not inputs:
+            raise GraphQLError('there is no parameter to update!')
+        elif inputs.answer and inputs.answer not in ['option1', 'option2', 'option3', 'option4']:
+            raise GraphQLError('choose one of the options as answer!')
+        else:
+            question_instance = Question.objects.get(id=id)
+            question_instance.question = inputs.question if inputs.question is not None else question_instance.question
+            question_instance.option1 = inputs.option1 if inputs.option1 is not None else question_instance.option1
+            question_instance.option2 = inputs.option2 if inputs.option2 is not None else question_instance.option2
+            question_instance.option3 = inputs.option3 if inputs.option3 is not None else question_instance.option3
+            question_instance.option4 = inputs.option4 if inputs.option4 is not None else question_instance.option4
+            question_instance.answer = inputs.answer if inputs.answer is not None else question_instance.answer
+            question_instance.save()
+            is_updated = True
+            return UpdateQuestion(is_updated=is_updated)
+
+
 class Mutate(graphene.ObjectType):
     create_question = CreateQuestion.Field()
+    update_question = UpdateQuestion.Field()
